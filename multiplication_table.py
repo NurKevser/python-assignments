964ed6ff29d463e42d2724e8a4d321c0ba9cf6d7
Kevser <nurkevserkadiroglu@gmail.com>
2021-04-22 12:48:44 +0300

    Multiplication Table

---
+++ multiplication_table.py
@@ -0,0 +1,16 @@
+
+x = int(input("enter a number between 1-10: "))
+for i in range(11):
+   print(f"{x}x{i} = {x*i}")
+enter a number: 5
+5x0 = 0
+5x1 = 5
+5x2 = 10
+5x3 = 15
+5x4 = 20
+5x5 = 25
+5x6 = 30
+5x7 = 35
+5x8 = 40
+5x9 = 45
+5x10 = 50
