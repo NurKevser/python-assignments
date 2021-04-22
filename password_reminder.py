1f8df8319a84425eb591a9d18403faf01d0ade5f
Kevser <nurkevserkadiroglu@gmail.com>
2021-04-22 12:37:06 +0300

    password_reminder

---
+++ password_reminder.py
@@ -0,0 +1,6 @@
+username = "kevser"
+name = input("Enter your username: ")
+if name == username:
+  print(f"Hello, {username}! The password is : W@12")
+else: 
+  print(f"Hello, {name}! See you later.")
