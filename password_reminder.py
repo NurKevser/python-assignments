ad538d31970dab53685377c40882d630b5ff8cb6
Kevser <nurkevserkadiroglu@gmail.com>
2021-04-24 01:51:32 +0300

    While döngüsü uyguladım

--- password_reminder.py
+++ password_reminder.py
@@ -1,6 +1,9 @@
 username = "kevser"
-name = input("Enter your username: ")
-if name == username:
-  print(f"Hello, {username}! The password is : W@12")
-else: 
-  print(f"Hello, {name}! See you later.")
+
+while name != username:
+  name=input("Enter your username: ")
+  if name == username:
+    print(f"Hello, {username}! The password is : W@12")
+  else: 
+    print(f"Hello! try again.")
+    
