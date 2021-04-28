9de350556e238e40558a5cdef004bc4d4296a45f
Kevser <nurkevserkadiroglu@gmail.com>
2021-04-28 15:32:01 +0300

    with list compherension

--- TwoSum.py
+++ TwoSum.py
@@ -3,3 +3,12 @@ def twoSum(target, nums):
                 for j in range(i+1, len(nums)):    #Bir sayıyı birden fazla 
                    if nums[i] + nums[j] == target: #kullanmasını engellemek için 
                        return [i, j]               #aralığı i+1 den başlatıyoruz
+
+
+
+                  WİTH LIST COMPHERENSION
+
+nums = [8,2,4,7,1,5,6,9]
+target = 16
+[(i,j) for i in range(len(nums)) for j in range(i+1, len(nums)) \
+ if nums[i]+nums[j] == target]                
