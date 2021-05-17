84f8d9b4f0f760419a717e3ed05a9c49dfde5b22
Kevser <nurkevserkadiroglu@gmail.com>
2021-05-17 12:09:56 +0300

    negative meaning

---
+++ negative_meaning.py
@@ -0,0 +1,19 @@
+Define a function to take a word and return negative meaning.
+Given a word, return a new word where "not " has been added to the front. However, if the word already begins with "not", return the string unchanged.
+
+For example:
+
+Test	Result
+print(not_string('sugar'))
+not sugar
+print(not_string('x'))
+not x
+print(not_string('not bad'))
+not bad
+
+
+def not_string(word):
+    if 'not' in word:
+        return word
+    else:
+        return 'not '+ word
